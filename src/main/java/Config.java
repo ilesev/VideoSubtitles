052ebf89a1e4ed723896cb4ad0c3115ff77a43e2
Iliyan Lesev <iliyanlesev@gmail.com>
2017-12-24 13:56:10 +0200

    Initial commit.

---
+++ src/main/java/Config.java
@@ -0,0 +1,5 @@
+public class Config {
+    public static final String dbUsername = "root";
+    public static final String dbPassword = "root";
+    public static final String dbUrl = "jdbc:mysql//localhost:3306/dbName";
+}
