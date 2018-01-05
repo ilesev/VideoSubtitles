b92e1344b524b439c10c117454bf5a8f12d036d4
ilesev <ilesev@shutterfly.com>
2018-01-05 15:05:42 +0200

    Added prerequisites to login

--- src/main/java/Servlets/LoginServlet.java
+++ src/main/java/Servlets/LoginServlet.java
@@ -1,5 +1,6 @@
 package Servlets;
 
+import Utils.DbUtils;
 import Utils.HttpUtils;
 
 import javax.servlet.ServletException;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
@@ -20,7 +22,17 @@ public class LoginServlet extends HttpServlet {
         if(session.getAttribute("username") != null) {
             HttpUtils.redirectToHome(request, response);
         } else {
-
+            try {
+                System.out.println(DbUtils.userExists(username));
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
         }
     }
 
--- src/main/java/Utils/DbUtils.java
+++ src/main/java/Utils/DbUtils.java
@@ -1,31 +1,31 @@
 package Utils;
 
 import java.sql.Connection;
+import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DbUtils {
     public static final String dbUsername = "root";
     public static final String dbPassword = "root";
     public static final String dbUrl = "jdbc:mysql//localhost:3306/VIDEO_SUBTITLES";
 
-    private static final String sqlUserSelect = "SELECT username FROM accounts WHERE username = ?";
+    private static final String sqlUserSelect = "SELECT username FROM accounts WHERE username = asd";
 
-    private static Connection getConnection() {
-        try(Connection connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword)) {
-            return connection;
-        } catch (SQLException e) {
-            return null;
-        }
+    private static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
     }
 
-    public static boolean userExists(String username) throws SQLException {
+    public static boolean userExists(String username) throws SQLException, ClassNotFoundException, IllegalAccessException, InstantiationException {
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
         Connection connection = getConnection();
-        PreparedStatement statement = connection.prepareStatement(sqlUserSelect);
-        statement.setString(1, username);
-        ResultSet resultSet = statement.executeQuery();
+        Statement statement = connection.createStatement();
+//        PreparedStatement statement = connection.prepareStatement(sqlUserSelect);
+//        statement.setString(1, username);
+        ResultSet resultSet = statement.executeQuery(sqlUserSelect);
         return resultSet.first();
     }
 }
