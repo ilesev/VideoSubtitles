8e9c283884511236ee718de356cf0f0aaa60a611
AlexMateva <alex.mateva@gmail.com>
2018-01-14 20:48:01 +0200

    convert to mp3

--- src/main/java/Servlets/HistoryServlet.java
+++ src/main/java/Servlets/HistoryServlet.java
@@ -14,6 +14,13 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
+import net.bramp.ffmpeg.FFmpeg;
+import net.bramp.ffmpeg.FFmpegExecutor;
+import net.bramp.ffmpeg.FFprobe;
+import net.bramp.ffmpeg.builder.FFmpegBuilder;
+import net.bramp.ffmpeg.probe.FFmpegFormat;
+import net.bramp.ffmpeg.probe.FFmpegProbeResult;
+import net.bramp.ffmpeg.probe.FFmpegStream;
 
 @WebServlet("/history")
 public class HistoryServlet extends HttpServlet{
@@ -31,6 +38,22 @@ public class HistoryServlet extends HttpServlet{
         HttpSession session = request.getSession();
         List<HistoryDTO> list = historyService.getUserHistory(session.getAttribute(Constants.PROPERTY_USERNAME).toString());
         request.setAttribute("list", list);
+
+
+        FFmpeg ffmpeg = new FFmpeg();
+        FFprobe ffprobe = new FFprobe();
+        FFmpegProbeResult probeResult = ffprobe.probe(Constants.FILE_SAVE_DIRECTORY + "How to keep your team motivated.mp4");
+
+        FFmpegBuilder builder = new FFmpegBuilder()
+                .setInput(Constants.FILE_SAVE_DIRECTORY + "How to keep your team motivated.mp4")     // Filename, or a FFmpegProbeResult
+                .overrideOutputFiles(false) // Override the output if it exists
+                .addOutput(Constants.FILE_SAVE_DIRECTORY  + "output.mp3")   // Filename for the destination
+                .setFormat("mp3")        // Format is inferred from filename, or can be set
+                .done();
+        FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
+
+        // Run a one-pass encode
+        executor.createJob(builder).run();
 
         request.getRequestDispatcher("/WEB-INF/jsp/history.jsp").forward(request, response);
     }
